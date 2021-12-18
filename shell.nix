4841acd575417c2a9ef85b8c3566a7dca6aad0a4
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-12-17 18:23:39 -0800

    Add prisma CLI to shell.nix

--- shell.nix
+++ shell.nix
@@ -1,5 +1,6 @@
-{ pkgs ? import <nixpkgs> {} }:
-  pkgs.mkShell {
-    # nativeBuildInputs is usually what you want -- tools you need to run
-    nativeBuildInputs = [ pkgs.nodejs-17_x pkgs.nodePackages.yarn ];
+{ pkgs ? import <nixpkgs> { } }:
+pkgs.mkShell {
+  # nativeBuildInputs is usually what you want -- tools you need to run
+  nativeBuildInputs =
+    [ pkgs.nodejs-17_x pkgs.nodePackages.yarn pkgs.nodePackages.prisma ];
 }

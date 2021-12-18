270a5f6b805b8ce54dfc0c821521ce7eb64fa567
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-12-17 19:02:23 -0800

    Fix prisma complaining about binaries on NixOS

--- shell.nix
+++ shell.nix
@@ -3,4 +3,11 @@ pkgs.mkShell {
   # nativeBuildInputs is usually what you want -- tools you need to run
   nativeBuildInputs =
     [ pkgs.nodejs-17_x pkgs.nodePackages.yarn pkgs.nodePackages.prisma ];
+  shellHook = with pkgs; ''
+    export PRISMA_MIGRATION_ENGINE_BINARY="${prisma-engines}/bin/migration-engine"
+    export PRISMA_QUERY_ENGINE_BINARY="${prisma-engines}/bin/query-engine"
+    export PRISMA_QUERY_ENGINE_LIBRARY="${prisma-engines}/lib/libquery_engine.node"
+    export PRISMA_INTROSPECTION_ENGINE_BINARY="${prisma-engines}/bin/introspection-engine"
+    export PRISMA_FMT_BINARY="${prisma-engines}/bin/prisma-fmt"
+  '';
 }

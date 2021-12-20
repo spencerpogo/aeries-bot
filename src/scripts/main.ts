e8fe3b8d4a743b9f43edc58c1823861f967b78e8
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-12-19 17:22:09 -0800

    Gracefully handle command errors

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,4 +1,4 @@
-import { Client } from "discord.js";
+import { Client, CommandInteraction } from "discord.js";
 import { COMMANDS } from "../allCommands.js";
 import { CONFIG } from "../config.js";
 import { CommandType } from "../types";
@@ -10,11 +10,24 @@ for (const cmd of COMMANDS) {
   commandsMap.set(cmd.meta.name, cmd);
 }
 
+async function handleCommandError(interaction: CommandInteraction, e: any) {
+  console.error(`Error in command ${interaction.commandName}`, e);
+  const msg = "An unexpected error occurred while running that command";
+  if (interaction.deferred || interaction.replied) {
+    await interaction.editReply(msg);
+  } else {
+    await interaction.reply(msg);
+  }
+}
+
 client.on("interactionCreate", async (interaction) => {
   if (interaction.isCommand()) {
     const cmd = await commandsMap.get(interaction.commandName);
-    if (cmd) {
+    if (!cmd) return;
+    try {
       await cmd.handler(interaction);
+    } catch (e) {
+      await handleCommandError(interaction, e);
     }
   }
 });

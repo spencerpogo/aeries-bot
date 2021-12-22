0fda415fb84fb1530767887d5cf0de73ad8da6f4
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-12-21 16:34:30 -0800

    Fix missing import

--- src/commands/notifications.ts
+++ src/commands/notifications.ts
@@ -1,4 +1,5 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 import { CommandInteraction } from "discord.js";
 import { prisma } from "../db.js";
 import { CommandType } from "../types";

642fe7621b6496db427f12e0cacca548233c04e0
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-12-26 17:05:12 -0800

    Add setup commands

--- src/allCommands.ts
+++ src/allCommands.ts
@@ -1,5 +1,11 @@
 import { HelloCommand } from "./commands/hello.js";
 import { LoginCommand } from "./commands/login.js";
 import { NotificationsCommand } from "./commands/notifications.js";
+import { SetupCommand } from "./commands/setup.js";
 
-export const COMMANDS = [HelloCommand, LoginCommand, NotificationsCommand];
+export const COMMANDS = [
+  HelloCommand,
+  LoginCommand,
+  NotificationsCommand,
+  SetupCommand,
+];
---
+++ src/commands/setup.ts
@@ -0,0 +1,27 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { CommandInteraction } from "discord.js";
+import { CommandType } from "../types";
+
+const MSG = [
+  "**Read before you use Aeries Bot**",
+  "1. This bot is **experimental** and subject to bugs. Please report any that you find" +
+    " so that they can be fixed!",
+  "2. You will be required to provide your Aeries password so the bot can log in to" +
+    " your account. **You are trusting the bot owner to not view your password.** If" +
+    " you would like to host it yourself ask I and I can show you how!",
+  "3. Use at your own risk! There is almost no chance you will get in any trouble for" +
+    " using this but bot owner is not responsible if you do!",
+  "",
+  "You can set up the bot using `/login`.",
+].join("\n");
+
+async function handler(interaction: CommandInteraction) {
+  await interaction.reply(MSG);
+}
+
+export const SetupCommand: CommandType = {
+  meta: new SlashCommandBuilder()
+    .setName("setup")
+    .setDescription("Provides info about using the bot."),
+  handler,
+};
